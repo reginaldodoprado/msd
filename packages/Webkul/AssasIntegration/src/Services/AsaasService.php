2f61aab820601b7422f497e1130c5cb7f3f2819d
Juan-Souzaa <souzajuan430@gmail.com>
2025-08-21 10:52:44 -0300

    Refatora o AsaasService para incluir novos serviços e métodos, como Pix, Cartão de Crédito e Negativações, além de reorganizar a injeção de dependências e adicionar métodos de acesso para cada novo serviço.

--- packages/Webkul/AssasIntegration/src/Services/AsaasService.php
+++ packages/Webkul/AssasIntegration/src/Services/AsaasService.php
@@ -4,39 +4,116 @@ namespace Ds\AssasIntegration\Services;
 
 class AsaasService
 {
+    protected $apiClient;
     protected $cobranca;
     protected $cliente;
     protected $assinatura;
     protected $notificacao;
     protected $transferencia;
     protected $webhook;
-    protected $relatorio;
-    protected $contaBancaria;
+    protected $pix;
+    protected $pixRecorrente;
+    protected $transacoesPix;
+    protected $cartaoCredito;
+    protected $parcelamento;
     protected $estorno;
-    protected $desconto;
+    protected $contaBancaria;
+    protected $linkPagamento;
+    protected $checkout;
+    protected $splits;
+    protected $antecipacoes;
+    protected $negativacoes;
+    protected $extrato;
+    protected $recargaCelular;
+    protected $pagamentoContas;
+    protected $notaFiscal;
+    protected $informacoesFiscais;
+    protected $informacoesFinanceiras;
+    protected $personalizarConta;
+    protected $subcontas;
+    protected $contaEscrow;
+    protected $documentoCobranca;
+    protected $cobrancaDadosResumidos;
+    protected $chargeBack;
+    protected $consultaSerasa;
+    protected $envioDocumentoWhiteLabel;
 
     public function __construct(
+        ApiClientService $apiClient,
         CobrancaService $cobranca,
         ClienteService $cliente,
         AssinaturaService $assinatura,
         NotificacaoService $notificacao,
         TransferenciaService $transferencia,
         WebhookService $webhook,
-        RelatorioService $relatorio,
-        ContaBancariaService $contaBancaria,
+        PixService $pix,
+        PixRecorrenteService $pixRecorrente,
+        TransacoesPixService $transacoesPix,
+        CartaoDeCreditoService $cartaoCredito,
+        ParcelamentoService $parcelamento,
         EstornoService $estorno,
-        DescontoService $desconto
+        ContaBancariaService $contaBancaria,
+        LinkDePagamentoService $linkPagamento,
+        CheckoutService $checkout,
+        SplitsService $splits,
+        AntecipacoesService $antecipacoes,
+        NegativacoesService $negativacoes,
+        ExtratoService $extrato,
+        RecargaDeCelularService $recargaCelular,
+        PagamentoDeContasService $pagamentoContas,
+        NotaFiscalService $notaFiscal,
+        InformacoesFiscaisService $informacoesFiscais,
+        InformacoesFinanceirasService $informacoesFinanceiras,
+        PersonalizarContaService $personalizarConta,
+        SubcontasService $subcontas,
+        ContaEscrowService $contaEscrow,
+        DocumentoDeCobrancaService $documentoCobranca,
+        CobrancaDadosResumidosService $cobrancaDadosResumidos,
+        ChargeBackService $chargeBack,
+        ConsultaSerasaService $consultaSerasa,
+        EnvioDeDocumentoWhiteLabelService $envioDocumentoWhiteLabel
     ) {
+        $this->apiClient = $apiClient;
         $this->cobranca = $cobranca;
         $this->cliente = $cliente;
         $this->assinatura = $assinatura;
         $this->notificacao = $notificacao;
         $this->transferencia = $transferencia;
         $this->webhook = $webhook;
-        $this->relatorio = $relatorio;
-        $this->contaBancaria = $contaBancaria;
+        $this->pix = $pix;
+        $this->pixRecorrente = $pixRecorrente;
+        $this->transacoesPix = $transacoesPix;
+        $this->cartaoCredito = $cartaoCredito;
+        $this->parcelamento = $parcelamento;
         $this->estorno = $estorno;
-        $this->desconto = $desconto;
+        $this->contaBancaria = $contaBancaria;
+        $this->linkPagamento = $linkPagamento;
+        $this->checkout = $checkout;
+        $this->splits = $splits;
+        $this->antecipacoes = $antecipacoes;
+        $this->negativacoes = $negativacoes;
+        $this->extrato = $extrato;
+        $this->recargaCelular = $recargaCelular;
+        $this->pagamentoContas = $pagamentoContas;
+        $this->notaFiscal = $notaFiscal;
+        $this->informacoesFiscais = $informacoesFiscais;
+        $this->informacoesFinanceiras = $informacoesFinanceiras;
+        $this->personalizarConta = $personalizarConta;
+        $this->subcontas = $subcontas;
+        $this->contaEscrow = $contaEscrow;
+        $this->documentoCobranca = $documentoCobranca;
+        $this->cobrancaDadosResumidos = $cobrancaDadosResumidos;
+        $this->chargeBack = $chargeBack;
+        $this->consultaSerasa = $consultaSerasa;
+        $this->envioDocumentoWhiteLabel = $envioDocumentoWhiteLabel;
+    }
+
+    /**
+     * Acessar serviço de API Client
+     */
+    public function api(): ApiClientService
+    {
+        return $this->apiClient;
     }
 
     /**
@@ -88,19 +165,43 @@ class AsaasService
     }
 
     /**
-     * Acessar serviço de relatórios
+     * Acessar serviço de PIX
      */
-    public function relatorio(): RelatorioService
+    public function pix(): PixService
     {
-        return $this->relatorio;
+        return $this->pix;
     }
 
     /**
-     * Acessar serviço de contas bancárias
+     * Acessar serviço de PIX recorrente
      */
-    public function contaBancaria(): ContaBancariaService
+    public function pixRecorrente(): PixRecorrenteService
     {
-        return $this->contaBancaria;
+        return $this->pixRecorrente;
+    }
+
+    /**
+     * Acessar serviço de transações PIX
+     */
+    public function transacoesPix(): TransacoesPixService
+    {
+        return $this->transacoesPix;
+    }
+
+    /**
+     * Acessar serviço de cartão de crédito
+     */
+    public function cartaoCredito(): CartaoDeCreditoService
+    {
+        return $this->cartaoCredito;
+    }
+
+    /**
+     * Acessar serviço de parcelamento
+     */
+    public function parcelamento(): ParcelamentoService
+    {
+        return $this->parcelamento;
     }
 
     /**
@@ -112,10 +213,162 @@ class AsaasService
     }
 
     /**
-     * Acessar serviço de descontos
+     * Acessar serviço de contas bancárias
      */
-    public function desconto(): DescontoService
+    public function contaBancaria(): ContaBancariaService
     {
-        return $this->desconto;
+        return $this->contaBancaria;
+    }
+
+    /**
+     * Acessar serviço de link de pagamento
+     */
+    public function linkPagamento(): LinkDePagamentoService
+    {
+        return $this->linkPagamento;
+    }
+
+    /**
+     * Acessar serviço de checkout
+     */
+    public function checkout(): CheckoutService
+    {
+        return $this->checkout;
+    }
+
+    /**
+     * Acessar serviço de splits
+     */
+    public function splits(): SplitsService
+    {
+        return $this->splits;
+    }
+
+    /**
+     * Acessar serviço de antecipações
+     */
+    public function antecipacoes(): AntecipacoesService
+    {
+        return $this->antecipacoes;
+    }
+
+    /**
+     * Acessar serviço de negativações
+     */
+    public function negativacoes(): NegativacoesService
+    {
+        return $this->negativacoes;
+    }
+
+    /**
+     * Acessar serviço de extrato
+     */
+    public function extrato(): ExtratoService
+    {
+        return $this->extrato;
+    }
+
+    /**
+     * Acessar serviço de recarga de celular
+     */
+    public function recargaCelular(): RecargaDeCelularService
+    {
+        return $this->recargaCelular;
+    }
+
+    /**
+     * Acessar serviço de pagamento de contas
+     */
+    public function pagamentoContas(): PagamentoDeContasService
+    {
+        return $this->pagamentoContas;
+    }
+
+    /**
+     * Acessar serviço de nota fiscal
+     */
+    public function notaFiscal(): NotaFiscalService
+    {
+        return $this->notaFiscal;
+    }
+
+    /**
+     * Acessar serviço de informações fiscais
+     */
+    public function informacoesFiscais(): InformacoesFiscaisService
+    {
+        return $this->informacoesFiscais;
+    }
+
+    /**
+     * Acessar serviço de informações financeiras
+     */
+    public function informacoesFinanceiras(): InformacoesFinanceirasService
+    {
+        return $this->informacoesFinanceiras;
+    }
+
+    /**
+     * Acessar serviço de personalizar conta
+     */
+    public function personalizarConta(): PersonalizarContaService
+    {
+        return $this->personalizarConta;
+    }
+
+    /**
+     * Acessar serviço de subcontas
+     */
+    public function subcontas(): SubcontasService
+    {
+        return $this->subcontas;
+    }
+
+    /**
+     * Acessar serviço de conta escrow
+     */
+    public function contaEscrow(): ContaEscrowService
+    {
+        return $this->contaEscrow;
+    }
+
+    /**
+     * Acessar serviço de documento de cobrança
+     */
+    public function documentoCobranca(): DocumentoDeCobrancaService
+    {
+        return $this->documentoCobranca;
+    }
+
+    /**
+     * Acessar serviço de cobrança dados resumidos
+     */
+    public function cobrancaDadosResumidos(): CobrancaDadosResumidosService
+    {
+        return $this->cobrancaDadosResumidos;
+    }
+
+    /**
+     * Acessar serviço de chargeback
+     */
+    public function chargeBack(): ChargeBackService
+    {
+        return $this->chargeBack;
+    }
+
+    /**
+     * Acessar serviço de consulta Serasa
+     */
+    public function consultaSerasa(): ConsultaSerasaService
+    {
+        return $this->consultaSerasa;
+    }
+
+    /**
+     * Acessar serviço de envio de documento white label
+     */
+    public function envioDocumentoWhiteLabel(): EnvioDeDocumentoWhiteLabelService
+    {
+        return $this->envioDocumentoWhiteLabel;
     }
 }
