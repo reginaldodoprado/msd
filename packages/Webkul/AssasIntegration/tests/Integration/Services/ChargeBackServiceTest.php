4a716527bcc7d61fced9eb6d3ace942468c0694d
Juan-Souzaa <souzajuan430@gmail.com>
2025-08-29 15:59:57 -0300

    Cria testes para o ChargeBackService.

---
+++ packages/Webkul/AssasIntegration/tests/Integration/Services/ChargeBackServiceTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Ds\AssasIntegration\Tests\Integration\Services;
+
+use Ds\AssasIntegration\Services\ApiClientService;
+use Ds\AssasIntegration\Services\ChargeBackService;
+use Tests\TestCase;
+
+class ChargeBackServiceTest extends TestCase
+{
+    protected $apiClient;
+    protected $chargeBackService;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        
+        $this->apiClient = app(ApiClientService::class);
+        $this->chargeBackService = new ChargeBackService($this->apiClient);
+    }
+
+    /** @test */
+    public function listar_chargebacks()
+    {
+        $filtros = [
+            'limit' => 10,
+            'offset' => 0
+        ];
+
+        $response = $this->chargeBackService->listarChargebacks($filtros);
+
+        dump('RESPOSTA LISTAR CHARGEBACKS:', $response);
+
+        $this->assertIsArray($response);
+        $this->assertArrayHasKey('data', $response);
+        $this->assertIsArray($response['data']);
+    }
+
+   
+}
